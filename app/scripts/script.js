2bac5809fcc1fb4b0b121ad4d46f9671d45a91f9
Becca Aaronson <becca.aaronson@gmail.com>
2014-09-17 17:55:23 -0500

    adding flexslider

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   $(".video-wrapper").fitVids();
+
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5
+  });
+  
 });
