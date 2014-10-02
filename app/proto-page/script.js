f70ec0feabe2c30f41fc5a5e3c9c061bbe2db6b3
Becca Aaronson <becca.aaronson@gmail.com>
2014-10-02 11:09:04 -0500

    move proto-page script

---
+++ app/proto-page/script.js
@@ -0,0 +1,22 @@
+// function setHeight() {
+//   windowHeight = $(window).innerHeight();
+//   $('.banner').css('min-height', windowHeight);
+// };
+
+
+$(document).ready(function() {
+  $(".video-wrapper").fitVids();
+
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 210,
+    itemMargin: 5
+  });
+
+  // setHeight();
+  
+  // $(window).resize(function() {
+  //   setHeight();
+  // });
+});
