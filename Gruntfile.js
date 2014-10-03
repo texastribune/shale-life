9d33e380f58ddd9beeef155a41c72546e373af51
Ryan Murphy <github@accounts.rdmurphy.org>
2014-10-03 14:49:13 -0500

    Total rehaul to move to app template

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+// Generated on 2014-10-03 using
+// generator-tt-newsapps 0.0.1
 'use strict';
 
 // # Globbing
@@ -6,7 +8,7 @@
 // Use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
@@ -14,17 +16,42 @@ module.exports = function(grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
+    // Configurable paths
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
+
   grunt.initConfig({
+
+    config: config,
 
     autoprefixer: {
       options: {
         browsers: ['last 1 version', 'ie 8', 'ie 9']
       },
       dist: {
-        files: {
-          './app/main.css': './app/main.css'
-        }
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
       }
+    },
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*'
+          ]
+        }]
+    },
+      server: '.tmp'
     },
 
     connect: {
@@ -36,50 +63,235 @@ module.exports = function(grunt) {
       },
       livereload: {
         options: {
-          base: ['./app'],
+          base: ['.tmp'],
           middleware: function(connect, options) {
             return [
               connect.static(options.base[0]),
-              connect().use('/bower_components', connect.static('./bower_components'))
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
             ];
           }
         }
       }
     },
 
+    copy: {
+      dist: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>',
+        dest: '<%= config.dist %>',
+        src: [
+          '*.{ico,png,txt}',
+          'images/{,*/}*.webp',
+          'styles/fonts/{,*/}*.*'
+        ]
+      },
+      compiledHtml: {
+        expand: true,
+        dot: true,
+        cwd: '.tmp',
+        dest: '<%= config.dist %>',
+        src: [
+          '{,*/}*.html',
+        ]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: '{,*/}*.html',
+          dest: '<%= config.dist %>'
+        }]
+      }
+    },
+
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          dest: '<%= config.dist %>/images'
+        }]
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+
+    nunjucks: {
+      options: {
+        data: grunt.file.readJSON('data.json')
+      },
+      render: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>',
+          src: ['{,*/}*.html', '!templates/*'],
+          dest: '.tmp',
+          ext: '.html'
+        }]
+      }
+    },
+
     sass: {
       options: {
         loadPath: [
-          './bower_components'
+          'bower_components'
         ]
       },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      },
       server: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      }
+    },
+
+    rev: {
+      dist: {
         files: {
-          './app/main.css': './app/main.scss'
+          src: [
+            '<%= config.dist %>/scripts/{,*/}*.js',
+            '<%= config.dist %>/styles/{,*/}*.css',
+            '<%= config.dist %>/images/{,*/}*.*',
+            '<%= config.dist %>/styles/fonts/{,*/}*.*',
+            '<%= config.dist %>/*.{ico,png}'
+          ]
         }
       }
     },
 
+    useminPrepare: {
+    options: {
+      dest: '<%= config.dist %>'
+    },
+      html: '.tmp/index.html'
+    },
+
+    usemin: {
+      options: {
+        assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
+      },
+      html: ['<%= config.dist %>/{,*/}*.html'],
+      css: ['<%= config.dist %>/styles/{,*/}*.css']
+    },
+
     watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
       gruntfile: {
         files: ['Gruntfile.js']
       },
-      sass: {
-        files: ['./**/*.{scss,sass}'],
-        tasks: ['sass:server', 'autoprefixer'],
+      html: {
+        files: ['<%= config.app %>/{,*/}*.html'],
+        tasks: ['nunjucks', 'wiredep'],
         options: {
           livereload: true
         }
+      },
+      js: {
+        files: ['<%= config.app %>/scripts/{,*/}*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '.tmp/styles/{,*/}*.css',
+          '<%= config.app %>/images/{,*/}*'
+        ]
+      },
+      sass: {
+        files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['sass:server', 'autoprefixer']
+      },
+    },
+
+    wiredep: {
+      app: {
+        src: ['.tmp/{,*/}*.html']
       }
     }
   });
 
   grunt.registerTask('serve', [
+    'clean:server',
+    'nunjucks',
+    'wiredep',
     'sass:server',
     'autoprefixer',
     'connect:livereload',
     'watch'
   ]);
+
+   grunt.registerTask('build', [
+    'clean:dist',
+    'nunjucks',
+    'wiredep',
+    'useminPrepare',
+    'sass:dist',
+    'copy:styles',
+    'imagemin',
+    'autoprefixer',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:dist',
+    'copy:compiledHtml',
+    'rev',
+    'usemin',
+    'htmlmin'
+    ]);
 
   grunt.registerTask('default', ['serve']);
 };
