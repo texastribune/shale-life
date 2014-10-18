04e4e2713310560bc0eb91b146051ef53b9663a8
Ryan Murphy <github@accounts.rdmurphy.org>
2014-10-18 14:18:45 -0500

    Add markdown parser to data builder

--- tools/build_data_json.js
+++ tools/build_data_json.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var marked = require('marked');
 var XLSX = require('xlsx');
 
 var SHEETS = [
@@ -38,9 +39,22 @@ SHEETS.forEach(function(sheet) {
     if (cell[0] === '!') { continue; }
     if (cell[0] === 'A') {
       var aCell = worksheet[cell];
-      var bCell = worksheet['B' + cell.match(/\d+/)[0]];
+      aCell = aCell ? aCell.v : '';
 
-      temp[aCell.v] = bCell ? bCell.v : '';
+      var cellNumber = cell.match(/\d+/)[0];
+
+      var bCell = worksheet['B' + cellNumber];
+      bCell = bCell ? bCell.v : '';
+
+      var cCell = worksheet['C' + cellNumber];
+      cCell = cCell ? cCell.v : '';
+
+
+      if (cCell === 'markdown') {
+        bCell = marked(bCell);
+      }
+
+      temp[aCell] = bCell;
     }
   }
 
