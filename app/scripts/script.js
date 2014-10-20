0c2d3505ce4ef55521581829d834811106522c3c
Becca Aaronson <becca.aaronson@gmail.com>
2014-10-20 17:22:38 -0500

    load youtube api on script.js, not base.html

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -88,11 +88,11 @@ $(document).ready(function() {
 
 });
 
-// var tag = document.createElement('script');
+var tag = document.createElement('script');
 
-// tag.src = 'https://www.youtube.com/iframe_api';
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // var player;
 
