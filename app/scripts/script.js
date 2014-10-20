c66af119949d209c7e1039c05a9ceec555415077
Becca Aaronson <becca.aaronson@gmail.com>
2014-10-20 17:17:42 -0500

    make fancybox work

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -85,6 +85,7 @@ $(document).ready(function() {
 
   // Set height of tan box based on size of content inside
   setHeight('.story-box', '.story');
+
 });
 
 // var tag = document.createElement('script');
@@ -112,6 +113,47 @@ $(document).ready(function() {
 //     }
 //   });
 // }
+
+// Fires whenever a player has finished loading
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// Fires when the player's state changes.
+function onPlayerStateChange(event) {
+    // Go to the next video after the current one is finished playing
+    if (event.data === 0) {
+        $.fancybox.next();
+    }
+}
+
+// The API will call this function when the page has finished downloading the JavaScript for the player API
+function onYouTubePlayerAPIReady() {
+
+  $(".fancybox")
+      .attr('rel', 'gallery')
+      .fancybox({
+          openEffect  : 'none',
+          closeEffect : 'none',
+          nextEffect  : 'none',
+          prevEffect  : 'none',
+          padding     : 0,
+          margin      : 50,
+          beforeShow  : function() {
+              // Find the iframe ID
+              var id = $.fancybox.inner.find('iframe').attr('id');
+
+              // Create video player object and add event listeners
+              var player = new YT.Player(id, {
+                  events: {
+                      'onReady': onPlayerReady,
+                      'onStateChange': onPlayerStateChange
+                  }
+              });
+          }
+      });
+
+}
 
 // Reset heights/positions on window resize
 $(window).resize(function() {
